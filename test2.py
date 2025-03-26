f703acdf03d6ec3b338b1daa2b737740d01d5a0c
loadthecode0 <artyaastha@gmail.com>
2025-03-26 11:26:22 +0530

    cleanup

--- test2.py
+++
@@ -1,7 +0,0 @@
-from lc_crf import LinearChainCRF, save_crf_model, load_crf_model
-
-c2 = LinearChainCRF()
-
-c2.fit('data/ner_train.csv', batchsize=50, numlines=50, show_tqdm=False, maxiter=20, train=True)
-# save_crf_model(c2, 'em_cond_50egs')
-c2.eval_from_file('data/ner_test.csv', numlines=10)
